29235d7049fb096e2dca4ed730aa720764e32309
taytum-twiss <twisstr@g.crookcountyschools.org>
2022-01-24 08:45:16 -0800

    worked on feature 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image'
+import Alert from 'react-bootstrap/Alert'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: "",
+      locationObj: {},
+      showError: false,
+      errorMessage: ""
+    };
+  }
+
+  handleChange = (event) => {
+    let typedCity = event.target.value;
+    this.setState({ city: typedCity });
+    console.log(typedCity);
+  };
+
+  getLocation = async (event) => {
+    event.preventDefault();
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`;
+    console.log('URL: ', url);
+    try {
+      let response = await axios.get(url);
+      console.log('Response: ', response.data[0]);
+      this.setState({
+        locationObj: response.data[0]
+      });
+    } catch(error) {
+      this.setState({
+        showError: true,
+        errorMessage: error.response.status + ': ' + error.response.data.error
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>City Explorer</h1>
+        </header>
+
+        <form className='form' onSubmit={this.getLocation}>
+          Your City:{" "}
+          <input type="text" name="yourcity" onChange={this.handleChange} />
+          <button type='submit'>Search</button>
+        </form>
+
+        {this.state.locationObj.display_name &&
+        <Container className='container'>
+          <h2>here is the map for {this.state.locationObj.display_name} </h2>
+          <p>Lat/Lon: {this.state.locationObj.lat},{this.state.locationObj.lon}</p>
+          <Image className='map' roundedCircle src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=12`} alt={this.state.locationObj.display_name} />
+        </Container>
+        }
+
+        {this.state.showError &&
+          <Alert variant='danger'>{this.state.errorMessage}</Alert>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
