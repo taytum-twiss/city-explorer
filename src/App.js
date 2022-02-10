b2a90de83a386e7020f656395e81d15e8cb08ea8
taytum-twiss <twisstr@g.crookcountyschools.org>
2022-02-10 08:47:56 -0800

    worked on feature 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
 import "./App.css";
-import axios from 'axios';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image'
-import Alert from 'react-bootstrap/Alert'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       city: "",
-      locationObj: {},
-      showError: false,
-      errorMessage: ""
     };
   }
 
@@ -24,20 +17,8 @@ class App extends React.Component {
 
   getLocation = async (event) => {
     event.preventDefault();
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`;
-    console.log('URL: ', url);
-    try {
-      let response = await axios.get(url);
-      console.log('Response: ', response.data[0]);
-      this.setState({
-        locationObj: response.data[0]
-      });
-    } catch(error) {
-      this.setState({
-        showError: true,
-        errorMessage: error.response.status + ': ' + error.response.data.error
-      })
-    }
+    const url = 'https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json';
+    console.log('URL:', url);
   }
 
   render() {
@@ -45,26 +26,14 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1>City Explorer</h1>
-        </header>
-
-        <form className='form' onSubmit={this.getLocation}>
+            </header>
+        <form onSubmit={this.getLocation}>
           Your City:{" "}
           <input type="text" name="yourcity" onChange={this.handleChange} />
-          <button type='submit'>Search</button>
+          <button>Search</button>
         </form>
-
-        {this.state.locationObj.display_name &&
-        <Container className='container'>
-          <h2>here is the map for {this.state.locationObj.display_name} </h2>
-          <p>Lat/Lon: {this.state.locationObj.lat},{this.state.locationObj.lon}</p>
-          <Image className='map' roundedCircle src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=12`} alt={this.state.locationObj.display_name} />
-        </Container>
-        }
-
-        {this.state.showError &&
-          <Alert variant='danger'>{this.state.errorMessage}</Alert>
-        }
-      </div>
+        <h2>here is the map for {this.state.city} </h2>
+        </div>
     );
   }
 }
